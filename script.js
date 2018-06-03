0c56604a4adafcdb45c025f1980c3f80ac3d7956
AndreCostaAIB <36609840+AndreCostaAIB@users.noreply.github.com>
2018-06-03 19:29:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,486 @@
+function refresh(){
+	location.reload();
+
+}
+
+function dois() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Um dos melhores anos da minha vida, pois não me lembro de nada!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "02.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function tres() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Muito semelhante ao ano anterior, sem qualquer recordação do mesmo.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "03.jpg");
+    x.setAttribute("width", "1536");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function quatro() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Lembro me apenas de jogar no meu GameBoy Advanced!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "04.jpg");
+    x.setAttribute("width", "768");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function cinco() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Desde pequeno adepto do melhor clube!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "05.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function seis() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Neste ano começou a viagem do ensino obrigatório, que está agora a acabar.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "06.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function sete() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Ano em que conheci um grande amigo!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "07.jpg");
+    x.setAttribute("width", "768");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function oito() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Saudades de uma pele limpa.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "08.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+}
+
+function nove() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "09.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Tudo o que eu tinha na cabeça era o filme 'Avatar'. ");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+  
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","09.mp3");
+    } else {
+        y.setAttribute("src","09.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+ } 
+
+function dez() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "10.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("O fim da primeira grande etapa da minha vida.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","10.mp3");
+    } else {
+        y.setAttribute("src","10.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function onze() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "11.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Sem qualquer vergonha!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","11.mp3");
+    } else {
+        y.setAttribute("src","11.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function doze() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "12.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Eu e a minha mãe.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","12.mp3");
+    } else {
+        y.setAttribute("src","12.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function treze() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "13.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("A peburdade estava a começar...");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","13.mp3");
+    } else {
+        y.setAttribute("src","13.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function quatorze() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "14.jpg");
+    x.setAttribute("width", "768");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px ");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("Uma grande mudança de um ano para o outro!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","14.mp3");
+    } else {
+        y.setAttribute("src","14.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function quinze() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "15.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("A família reunida.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","15.mp3");
+    } else {
+        y.setAttribute("src","15.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);	
+}
+
+function dezasseis() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "16.jpg");
+    x.setAttribute("width", "1024");
+    x.setAttribute("height", "768");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("O acne já atacava.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","16.mp3");
+    } else {
+        y.setAttribute("src","16.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function dezassete() {
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "17.jpg");
+    x.setAttribute("width", "768");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("A minha segunda competição de Jiu Jitsu Brasileiro.");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","17.mp3");
+    } else {
+        y.setAttribute("src","17.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+}
+
+function dezoito(){
+	var z = document.createElement("TABLE");
+    z.setAttribute("id", "myTable");
+    document.body.appendChild(z);
+	var a = document.createElement("TR");
+    a.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(a);
+	var b = document.createElement("TD");
+    document.getElementById("myTr").appendChild(b);
+	
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "18.jpg");
+    x.setAttribute("width", "768");
+    x.setAttribute("height", "1024");
+    document.body.appendChild(x);
+	x.setAttribute("border","5px");
+	
+	var h = document.createElement("H3")               
+	var t = document.createTextNode("O atual 'eu'!");     
+	h.appendChild(t);                                   
+	document.body.appendChild(h);
+	
+	var y = document.createElement("AUDIO");
+    if (y.canPlayType("audio/mpeg")) {
+        y.setAttribute("src","18.mp3");
+    } else {
+        y.setAttribute("src","18.ogg");
+    }
+    y.setAttribute("controls", "controls");
+    document.body.appendChild(y);
+	
+}
